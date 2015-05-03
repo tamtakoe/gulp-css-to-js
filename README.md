bdb81a0611e767a2eed3ec3ba281df5192c20c1a
tamtakoe <tamtakoe@gmail.com>
2015-05-04 01:05:44 +0300

    fix readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# gulp-css-to-js
+# gulp-css-to-js [![NPM version](https://badge.fury.io/js/gulp-css-to-js.svg)](http://badge.fury.io/js/gulp-css-to-js)
+
+> gulp plugin which convert css-file to js-file which makes style tag
+
+## Install with [npm](npmjs.org)
+
+```sh
+npm install gulp-css-to-js
+```
+
+## Usage
+
+```js
+var cssToJs = require('gulp-css-to-js');
+
+var scriptStream = gulp.src('src/**/*.js');
+    
+var styleStream = gulp.src('src/**/*.css')
+    .pipe(cssToJs());
+    
+merge(scriptStream, styleStream)
+    .pipe(concat('script.js'))
+//the script.js will insert styles in DOM by running document.createElement('style')
+```
+
+
+## License
+
+Copyright (c) 2014-2015 Oleg Istomin
+Released under the MIT license
+
+***
---
+++ index.js
@@ -0,0 +1,22 @@
+var path      = require("path");
+var through   = require('through2');
+var escapeStr = require('js-string-escape');
+
+var STYLE_HEADER = '!function(){var a="';
+var STYLE_FOOTER = '",b=document.createElement("style");b.type="text/css",b.styleSheet?b.styleSheet.cssText=a:b.appendChild(document.createTextNode(a)),(document.head||document.getElementsByTagName("head")[0]).appendChild(b)}();';
+
+function cssTojs() {
+    function transform(file, enc, callback) {
+
+        var css = file.contents.toString();
+        var content = STYLE_HEADER + escapeStr(css) + STYLE_FOOTER;
+
+        file.contents = new Buffer(content);
+
+        callback(null, file);
+    }
+
+    return through.obj(transform);
+}
+
+module.exports = cssTojs;
